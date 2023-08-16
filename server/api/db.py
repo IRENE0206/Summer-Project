27923137e7083a04c0ed8c9edf72478e65e41bbc
Irene Jiaxin Fan <irene0206@outlook.com>
2023-08-16 15:02:58 +0100

    Made modifications to the db.py to better line up with flask_sqlalchemy 3.0

--- server/api/db.py
+++ server/api/db.py
@@ -1,49 +1,41 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, Text, CheckConstraint
-from sqlalchemy.dialects.sqlite import DATE
 
 # create the extension
 db = SQLAlchemy()
 
-
-class Users(db.Model):
-    __tablename__ = "users"
-    user_id = db.Column(Integer, primary_key=True, autoincrement=True)
-    username = db.Column(Text, unique=True, nullable=False)
-    password = db.Column(Text, nullable=False)
-    role = db.Column(Text, nullable=False)
+class Users(db.Model):  
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)  
+    username = db.Column(db.String, unique=True, nullable=False)  
+    password = db.Column(db.String, nullable=False)  
+    role = db.Column(db.String, nullable=False)  
     __table_args__ = (
-        CheckConstraint("role IN ('admin', 'regular')"),
+        db.CheckConstraint("role IN ('admin', 'regular')"),  
     )
 
 
-class Workbooks(db.Model):
-    __tablename__ = "workbooks"
-    workbook_id = db.Column(Integer, primary_key=True, autoincrement=True)
-    workbook_name = db.Column(Text, unique=True, nullable=False)
-    release_date = db.Column(DATE)
-    last_edit = db.Column(DATE, nullable=False)
+class Workbooks(db.Model): 
+    workbook_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    workbook_name = db.Column(db.String, unique=True)  
+    release_date = db.Column(db.Date) 
+    last_edit = db.Column(db.Date, nullable=False) 
 
 
-class Exercises(db.Model):
-    __tablename__ = "exercises"
-    exercise_id = db.Column(Integer, primary_key=True, autoincrement=True)
-    exercise_number = db.Column(Text, nullable=False)
-    exercise_content = db.Column(Text, nullable=False)
-    workbook_id = db.Column(Integer, db.ForeignKey("workbooks.workbook_id"), nullable=False)
+class Exercises(db.Model): 
+    exercise_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    exercise_number = db.Column(db.String) 
+    exercise_content = db.Column(db.String) 
+    workbook_id = db.Column(db.Integer, db.ForeignKey("workbooks.workbook_id"), nullable=False) 
 
 
-class Answers(db.Model):
-    __tablename__ = "answers"
-    answer_id = db.Column(Integer, primary_key=True, autoincrement=True)
-    feedback = db.Column(Text)
-    exercise_id = db.Column(Integer, db.ForeignKey("exercises.exercise_id"), nullable=False)
-    user_id = db.Column(Integer, db.ForeignKey("users.user_id"), nullable=False)
+class Answers(db.Model): 
+    answer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    feedback = db.Column(db.String)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercises.exercise_id"), nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)  
 
-class Lines(db.Model):
-    __tablename__ = "lines"
-    line_id = db.Column(Integer, primary_key=True, autoincrement=True)
-    line_index = db.Column(Integer)
-    answer_id = db.Column(Integer, db.ForeignKey("answers.answer_id"), nullable=False)
-    variable = db.Column(Text)
-    rules = db.Column(Text)
+class Lines(db.Model): 
+    line_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    line_index = db.Column(db.Integer, nullable=False) 
+    answer_id = db.Column(db.Integer, db.ForeignKey("answers.answer_id"), nullable=False)  
+    variable = db.Column(db.String) 
+    rules = db.Column(db.String)
