9bc7823e1c09f6b4c865f254fbec661c3d94d60c
Irene Jiaxin Fan <irene0206@outlook.com>
2023-08-16 19:06:03 +0100

    Optimized db.py for readability

--- server/api/db.py
+++ server/api/db.py
@@ -1,42 +1,57 @@
 from flask_sqlalchemy import SQLAlchemy
 from .util import UserRole
 
-# create the extension
+# Create the extension
 db = SQLAlchemy()
+
 
 class User(db.Model):  
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)  
     username = db.Column(db.String, unique=True, nullable=False)  
     password = db.Column(db.String, nullable=False)  
     role = db.Column(db.Enum(UserRole), nullable=False)
+
+    # Relationships
     answers = db.relationship("Answer", back_populates="user")
+
+    # Ensure the role is one of the UserRole values
     __table_args__ = (
         db.CheckConstraint(f"role IN ({', '.join([f'{repr(role.value)}' for role in UserRole])})"),  
     )
+
 
 class Workbook(db.Model): 
     workbook_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
     workbook_name = db.Column(db.String, unique=True, nullable=False)  
     release_date = db.Column(db.Date) 
     last_edit = db.Column(db.Date, nullable=False) 
+
+    # Relationships
     exercises = db.relationship("Exercise", back_populates="workbook")
+
 
 class Exercise(db.Model): 
     exercise_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     exercise_number = db.Column(db.String, nullable=False) 
     exercise_content = db.Column(db.String, nullable=False) 
     workbook_id = db.Column(db.Integer, db.ForeignKey("workbook.workbook_id"), nullable=False)
+
+    # Relationships
     workbook = db.relationship("Workbook", back_populates="exercises")
     answers = db.relationship("Answer", back_populates="exercise")
+
 
 class Answer(db.Model): 
     answer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     feedback = db.Column(db.String, nullable=False)
     exercise_id = db.Column(db.Integer, db.ForeignKey("exercise.exercise_id"), nullable=False)  
     user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
+
+    # Relationships
     user = db.relationship("User", back_populates="answers")
-    exercise = db.relationship("Exercise", back_populates="answer")
+    exercise = db.relationship("Exercise", back_populates="answers")
     lines = db.relationship("Line", back_populates="answer")
+
 
 class Line(db.Model): 
     line_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -44,4 +59,6 @@ class Line(db.Model):
     answer_id = db.Column(db.Integer, db.ForeignKey("answer.answer_id"), nullable=False)  
     variable = db.Column(db.String, nullable=False) 
     rules = db.Column(db.String, nullable=False)
+
+    # Relationships
     answer = db.relationship("Answer", back_populates="lines")
